4332fa974adca817dbf421bacac58a1c1d42db27
Jean-Mark Wright <jeanmark.wright@gmail.com>
2013-03-31 23:22:22 -0500

    Added commands to find references, load stuff from gazetteer and refine references

---
+++ masters/articles/management/commands/refine_references.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.core.management import call_command
+from datetime import timedelta, date, datetime
+from django.conf import settings
+from django.contrib.auth.models import Group, Permission
+import os
+import threading
+from masters.articles.models import *
+
+class Command(BaseCommand):
+    args = ''
+    help = 'Loads some test data'
+
+    def handle(self, *args, **options):
+    	""" 
+    	This finds references for us
+    	"""
+        Gazetteer.refine_references()
