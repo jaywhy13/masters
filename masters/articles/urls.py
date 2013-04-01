14f926f50f52a167df2b3b7dacf59752396be198
Jean-Mark Wright <jeanmark.wright@gmail.com>
2013-03-31 23:23:46 -0500

    Added in urls and views for the reference_context and article stats

--- masters/articles/urls.py
+++ masters/articles/urls.py
@@ -1,11 +1,16 @@
 from django.conf.urls import patterns, include, url
-from views import review, update_reference, close_article
+from views import review, update_reference, close_article, preview_article, reference_context, article_statistics
+from django.views.generic.simple import direct_to_template
 from models import *
 
 urlpatterns = patterns('',
+	url(r'preview_article/id/(?P<id>\d+)$', preview_article, name='preview-article'),
     url(r'review_article/id/(?P<id>\d+)$', review, {'model':Article, 'json_name':'article', 'template_name' : 'article.html'}, name='review-article' ),
     url(r'close_article/id/(?P<id>\d+)$', close_article, name='close-article'),
+    url(r'statistics$', article_statistics, name='article-statistics'),
+    # References...
     url(r'review_reference/id/(?P<id>\d+)$', review, {'model':ArticleReference, 'json_name':'article_reference', 'template_name': 'article_reference.html'}, name='review-reference'),
     url(r'confirm_reference/id/(?P<id>\d+)$', update_reference, {'action' : 'confirm'}, name='confirm-reference'),
     url(r'remove_reference/id/(?P<id>\d+)$', update_reference, {'action' : 'remove'}, name='remove-reference'),
+    url(r'reference_context/id/(?P<id>\d+)$', reference_context, name='reference-context')
 )
--- masters/articles/views.py
+++ masters/articles/views.py
@@ -4,6 +4,11 @@ from django.template import RequestContext
 from django.shortcuts import get_object_or_404, render_to_response
 from models import *
 
+
+def preview_article(request, id):
+	obj = get_object_or_404(Article, pk=id)
+	return render_to_response("preview_article.html", context_instance=RequestContext(request, dict(article=obj)))
+
 def review(request, id, model=None, json_name=None, template_name=None):
 	""" Returns the next article that we can verify the references for
 	"""
@@ -16,9 +21,11 @@ def update_reference(request, id, action="confirm"):
 	article_reference = get_object_or_404(ArticleReference, pk=id)
 	if action == "confirm":
 		article_reference.confirmed = True
+		article_reference.valid = True
 		article_reference.save()
 	elif action == "remove":
-		article_reference.delete()
+		article_reference.confirmed = True
+		article_reference.save()
 	return HttpResponse("success")
 
 def close_article(request, id):
@@ -26,3 +33,13 @@ def close_article(request, id):
 	article.reviewed = True
 	article.save()
 	return HttpResponse("success")
+
+def reference_context(request, id):
+	article_reference = get_object_or_404(ArticleReference, pk=id)
+	return render_to_response('reference_context.html', context_instance=RequestContext(request, dict(article_reference=article_reference)))
+
+def article_statistics(request):
+	article_count = Article.objects.filter(reviewed=True).count()
+	confirmed_reference_count = ArticleReference.objects.filter(confirmed=True).count()
+	return HttpResponse("%s articles closed, %s references confirmed" % (article_count, confirmed_reference_count))
+
