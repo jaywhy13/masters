34a2a32b39dffc72edfc77d299220c316e28c99f
Jean-Mark Wright <jeanmark.wright@gmail.com>
2013-04-07 12:04:27 -0500

    Added article and reference admin

--- masters/articles/admin.py
+++ masters/articles/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 import models
 
+class ArticleReferenceInline(admin.TabularInline):
+	model = models.ArticleReference
+
 class ArticleAdmin(admin.ModelAdmin):
     model = models.Article
+    search_fields = ['title', 'body']
+    inlines = [ ArticleReferenceInline ]
+
+class ArticleReferenceAdmin(admin.ModelAdmin):
+	model = models.ArticleReference
+	search_fields = ['gazetteer__name', 'article__title', 'article__body']
+	list_display = ['confirmed', 'article', 'gazetteer', 'sentence', 'position_in_sentence', 'valid']
+	ordering = ['confirmed', 'valid', 'gazetteer__name']
+
+	def sentence(self, model):
+		return model.sentence
+
 
 admin.site.register(models.Article, ArticleAdmin)
+admin.site.register(models.ArticleReference, ArticleReferenceAdmin)
