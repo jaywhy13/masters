c39a7f84b555cd547a62f4ad96e2278247063da6
Jean-Mark Wright <jeanmark.wright@gmail.com>
2013-04-07 12:09:39 -0500

    Fetched the article ID if any from the GET parameter for the index.html page

--- masters/views.py
+++ masters/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 def home(request):
-	return render_to_response("index.html", context_instance=RequestContext(request))
+	article_id = request.GET.get("article_id", None)
+	return render_to_response("index.html", context_instance=RequestContext(request, locals()))
